3260af96243e01da7813133ccbf3ff2e34eb7af0
Yasin Abdullah Hassan <yhassan3gmu@gmail.com>
2023-03-05 21:47:27 -0500

    Update python_dictionary_manipulation6.py

--- python_dictionary_manipulation6.py
+++ python_dictionary_manipulation6.py
@@ -1,7 +1,7 @@
 import json
 from datetime import datetime
 '''
-This file Uses Python dictionary manipulation for dictionaries in a JSON file, which contains user IDs, URLs, date, time spent on website. Displayed features such as
+This file Uses Python dictionary manipulation for dictionaries in a JSON file, which contains user IDs, URLs, date, time spent on website. The results will be
     ●	Unique URLs visited by each user. 
     ●	Unique URLs visited by each user where user ID is inputted.
     ●	Unique URLs visited on a certain date for each user. 
